71976a3e6c03ba084f0f08f9c1d9a2a50bc5122c
Yvonne Hopkins <yveehopkins@gmail.com>
2018-11-29 19:39:42 -0800

    added index articles

--- index.php
+++ index.php
@@ -16,6 +16,28 @@
                 <img src="images/index/computerhand-medium.jpeg" alt="laptop on desk">
                 <h2>COMP 1950</h2>
             </div>
+
+            <div class="mainpagearticles">
+
+            <article>
+                <h2><a href="#0">Outline</a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </article>
+
+            <article>
+                <h2><a href="collection.html">Schedule</a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </article>
+
+            <article>
+                <h2><a href="#0">Lessons</a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </article>
+
+            </div>
         </main>
 
         <?php include "./templates/footer.php"?>
