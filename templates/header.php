93fae90fd805465dc7686100e035d3186b92ab02
Yvonne Hopkins <yveehopkins@gmail.com>
2018-11-30 19:44:55 -0800

    made logo clickable

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <header>
-    <img src="images/logo.png" alt="bcitlogo">
+    <a href="index.php" class="logoa"><img src="images/logo.png" alt="bcitlogo"></a>
     <button class="btn-menu">Menu</button>
     <?php include './templates/nav.php'?>
 </header>
