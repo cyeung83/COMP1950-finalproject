01000a26276147221188cf2e7b092566a27503c9
Yvonne Hopkins <yveehopkins@gmail.com>
2018-11-30 18:56:59 -0800

    lessons landing page styles

--- lessons.php
+++ lessons.php
@@ -23,31 +23,31 @@
                 <a href="lesson3.php" class="lesson lesson-3">
                     <p>Lesson 3</p>
                 </a>
-                <a href="lesson4.html" class="lesson lesson-4">
+                <a href="#0" class="lesson lesson-4">
                     <p>Lesson 4</p>
                 </a>
-                <a href="lesson5.html" class="lesson lesson-5">
+                <a href="#0" class="lesson lesson-5">
                     <p>Lesson 5</p>
                 </a>
-                <a href="lesson6.html" class="lesson lesson-6">
+                <a href="#0" class="lesson lesson-6">
                     <p>Lesson 6</p>
                 </a>
-                <a href="lesson7.html" class="lesson lesson-7">
+                <a href="#0" class="lesson lesson-7">
                     <p>Lesson 7</p>
                 </a>
-                <a href="lesson8.html" class="lesson lesson-8">
+                <a href="#0" class="lesson lesson-8">
                     <p>Lesson 8</p>
                 </a>
-                <a href="lesson9.html" class="lesson lesson-9">
+                <a href="#0" class="lesson lesson-9">
                     <p>Lesson 9</p>
                 </a>
-                <a href="lesson10.html" class="lesson lesson-10">
+                <a href="#0" class="lesson lesson-10">
                     <p>Lesson 10</p>
                 </a>
-                <a href="lesson11.html" class="lesson lesson-11">
+                <a href="#0" class="lesson lesson-11">
                     <p>Lesson 11</p>
                 </a>
-                <a href="lesson12.html" class="lesson lesson-12">
+                <a href="#0" class="lesson lesson-12">
                     <p>Lesson 12</p>
                 </a>
 
