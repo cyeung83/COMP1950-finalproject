766263f534c4ad31df7b009ad25353801232e2b3
Yvonne Hopkins <yveehopkins@gmail.com>
2018-11-30 20:28:54 -0800

    grid footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,19 @@
 <footer>
-    <p>Footer content</p>
-    <p> blah blah blah blah </p>
+    <div class="row">
+        <div class="one-third column">
+            <h2><a href="index.php">BCIT</a></h2>
+        </div>
+        <div class="one-third column">
+            <h2 class="credits">Visit Us</h2>
+            <p>British Columbia Institute of Technology</p>
+            <p>3700 Willingdon Avenue</p>
+            <p>Burnaby, British Columbia</p>
+            <p>Canada, V5G 3H2</p>
+        </div>
+        <div class="one-third column">
+            <h2 class="contact">Contact Us</h2>
+            <p>Telephone: (604) 434-5734</p>
+          <p>Toll-free (CAN/US): 1-866-434-1610</p>
+        </div>
+    </div>
 </footer>
