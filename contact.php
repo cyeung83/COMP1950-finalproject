d40021b80efcad9a9c3b1ea17eddeffa947e019b
CYeung <cyeung83@my.bcit.ca>
2018-11-30 16:29:32 -0800

    Started styling the contact page...

--- contact.php
+++ contact.php
@@ -27,22 +27,25 @@
                         </tr>
                     </table>
                 </section>
+                
                 <div class=form-wrapper>
+                    <h2>Got questions? Send us a message!</h2>
                     <form>
-                        <label for="firstname">First Name:</label>
+                        <label class="formfield-span">Name</label>
                         <input type="text" id="firstname" name="firstname" placeholder="First Name" required>
-                        <label for="lastname">Last Name:</label>
                         <input type="text" id="lastname" name="lastname" placeholder="Last Name" required>
-                        <label for="email">E-mail Address:</label>
-                        <input type="email" id="email" name="email" placeholder="E-mail" required>
-                        <label for="studentnumber">BCIT Student number</label>
-                        <input type="text" id="studentnumber" name="studentnumber" placeholder="Student Number">
-                        <input type="radio" id="prospective-radio" name="studenttype" value="prospective">
-                        <label for="prospective-radio">Prospective Student</label>
-                        <input type="radio" id="current-radio" name="studenttype" value="current">
-                        <label for="current-radio">Current Student</label>
-                        <textarea name="message" rows=10 placeholder="Message" required></textarea>
-                        <button type="submit">Send</button>
+                        <label class="formfield-span" for="email">E-mail</label>
+                        <input class="formfield-span" type="email" id="email" name="email" placeholder="E-mail" required>
+                        <label class="formfield-span" for="studentnumber">BCIT Student number</label>
+                        <input class="formfield-span" type="text" id="studentnumber" name="studentnumber" placeholder="Student Number">
+                        <div class="radio-wrapper formfield-span">
+                            <input type="radio" id="prospective-radio" name="studenttype" value="prospective">
+                            <label for="prospective-radio">Prospective Student</label>
+                            <input type="radio" id="current-radio" name="studenttype" value="current">
+                            <label for="current-radio">Current Student</label>
+                        </div>
+                        <textarea class="formfield-span" name="message" rows=10 placeholder="Message" required></textarea>
+                        <button class="formsubmitbtn formfield-span" type="submit">Send</button>
                     </form>
                 </div>
             </main>
