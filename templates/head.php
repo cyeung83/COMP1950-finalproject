6aac8c58f4403dfb662192783004d9ecafd3f4b7
Yvonne Hopkins <yveehopkins@gmail.com>
2018-12-03 18:26:23 -0800

    styles5

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,5 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>COMP1950-<?php echo $pageName;?></title>
-    <link rel="stylesheet" href="styles4/styles.css">
+    <link rel="stylesheet" href="styles5/styles.css">
 </head>
