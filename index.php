2cd4eb78eedc8c3d40e75ab106e9f4dddb9037f3
Yvonne Hopkins <yveehopkins@gmail.com>
2018-12-03 20:25:47 -0800

    index button fix

--- index.php
+++ index.php
@@ -23,21 +23,21 @@
                 <h2><a href="#0">Outline</a></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <button href="#" class="myButton">Learn more</button>
+                <button href="outline.php" class="myButton">Learn more</button>
             </article>
 
             <article>
                 <h2><a href="collection.html">Schedule</a></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <button href="#" class="myButton">Learn more</button>
+                <button href="schedule.php" class="myButton">Learn more</button>
             </article>
 
             <article>
                 <h2><a href="#0">Lessons</a></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <button href="#" class="myButton">Learn more</button>
+                <button href="lessons.php" class="myButton">Learn more</button>
             </article>
 
             </div>
