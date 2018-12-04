b58fa0f4dfb68a24fd9a2eb2b45fd2b44263869c
CYeung <cyeung83@my.bcit.ca>
2018-12-03 20:01:41 -0800

    Add favicon

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>COMP1950-<?php echo $pageName;?></title>
     <link rel="stylesheet" href="styles7/styles.css">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 </head>
