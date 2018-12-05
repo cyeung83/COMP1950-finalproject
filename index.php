6939c60a1f31804b6fead918968dd62320d29a34
Yvonne Hopkins <yveehopkins@gmail.com>
2018-12-04 19:47:04 -0800

    code validator

--- index.php
+++ index.php
@@ -20,21 +20,21 @@
             <div class="mainpagearticles">
 
             <article>
-                <h2><a href="#0">Outline</a></h2>
+                <h2>Outline</a></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <a href="outline.php"><button class="myButton">Learn more</button><a>
             </article>
 
             <article>
-                <h2><a href="collection.html">Schedule</a></h2>
+                <h2>Schedule</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <a href="schedule.php"><button class="myButton">Learn more</button></a>
             </article>
 
             <article>
-                <h2><a href="#0">Lessons</a></h2>
+                <h2>Lessons</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <a href="lessons.php"><button class="myButton">Learn more</button></a>
