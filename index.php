7f3af44cdd7bb844070337ac0686dc352e2ae1ec
Yvonne Hopkins <yveehopkins@gmail.com>
2018-12-04 20:17:18 -0800

    validation index page

--- index.php
+++ index.php
@@ -23,21 +23,21 @@
                 <h2>Outline</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <button class="myButton"><a href="outline.php">Learn more</a></button>
+                <a href="outline.php"><button class="myButton">Learn more</button></a>
             </article>
 
             <article>
                 <h2>Schedule</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <button class="myButton"><a href="schedule.php">Learn more</a></button>
+                <a href="schedule.php"><button class="myButton">Learn more</button></a>
             </article>
 
             <article>
                 <h2>Lessons</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <button class="myButton"><a href="lessons.php">Learn more</a></button>
+                <a href="lessons.php"><button class="myButton">Learn more</button></a>
             </article>
 
             </div>
